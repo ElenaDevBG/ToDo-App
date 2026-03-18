ad05194632c95fd7cd81f001ca0bfc68c6bbcc36
Elena Choteva <elenachoteva@gmail.com>
2026-03-18 12:00:06 +0200

    Add new bonus file

--- web.py
+++ web.py
@@ -13,8 +13,13 @@ st.title("My To-Do App")
 st.subheader("This is my to do app.")
 st.write("This app is to increase my productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Enter a todo:", placeholder="Add new todo...", on_change=add_todo, key="new_todo")
 
